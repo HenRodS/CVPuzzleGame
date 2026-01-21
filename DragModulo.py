1e0c02f0540396465a74c67b5c48589d20c94c18
HenRodS <henrique.karekas@gmail.com>
2026-01-21 18:31:18 -0300

    Feature: adição de uma imagem alvo para cada peça

--- DragModulo.py
+++ DragModulo.py
@@ -3,27 +3,33 @@ import cvzone
 import random
 
 class DragImg():
-    def __init__(self, path, posOrigin, posTarget, imgType, scale=1.0):
+    def __init__(self, path, pathTarget, posOrigin, posTarget, imgType, width=200, height=200):
         self.path = path
+        self.pathTarget = pathTarget
         self.posOrigin = posOrigin  # Onde a imagem está agora
         self.posTarget = posTarget  # Onde ela deve ser encaixada
         self.imgType = imgType
         self.isMatched = False  # Indica se já encaixou
 
+        # Carrega a imagem
         if self.imgType == 'png':
             self.img = cv2.imread(self.path, cv2.IMREAD_UNCHANGED)
+            self.imgTarget = cv2.imread(self.pathTarget, cv2.IMREAD_UNCHANGED)
         else:
             self.img = cv2.imread(self.path)
+            self.imgTarget = cv2.imread(self.pathTarget)
 
-        if scale != 1.0:
-            h, w = self.img.shape[:2]
-            newSize = (int(w * scale), int(h * scale))
-            self.img = cv2.resize(self.img, newSize, interpolation=cv2.INTER_AREA)
+        # 2. Redimensiona para o tamanho padrão em pixels
+        novo_tamanho = (width, height)  # (Largura, Altura)
+        novo_tamanho_target = (width + 10, height + 10)
+        self.img = cv2.resize(self.img, novo_tamanho, interpolation=cv2.INTER_AREA)
+        self.imgTarget = cv2.resize(self.imgTarget, novo_tamanho_target, interpolation=cv2.INTER_AREA)
 
         # Gera uma cor aleatoria
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-        self.size = self.img.shape[:2]
+        self.size = self.img.shape[:2] # Tamanho da imagem
+        self.sizeTarget = self.imgTarget.shape[:2]  # Tamanho da imagem_alvo (pode ser diferente)
 
     def update(self, cursor):
         if self.isMatched: return  # Se já encaixou, não move mais
@@ -39,5 +45,5 @@ class DragImg():
             tx, ty = self.posTarget
             # Se a distância for menor que 50 pixels, encaixa no alvo
             if abs(self.posOrigin[0] - tx) < 50 and abs(self.posOrigin[1] - ty) < 50:
-                self.posOrigin = [tx, ty]
+                self.posOrigin = [tx + 5, ty + 5]
                 self.isMatched = True
